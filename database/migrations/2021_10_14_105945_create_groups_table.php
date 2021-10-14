4ec252d0715b7578a1c5a306e2f942ec5bd40dcb
Hamzah Shajrawi <hamzahshajrawi@gmail.com>
2021-10-14 15:04:34 +0300

    all tables

--- database/migrations/2021_10_14_105945_create_groups_table.php
+++ database/migrations/2021_10_14_105945_create_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->date('dob');
+            $table->integer('admin_id')->default(0);
             $table->integer('type')->default(0);
             $table->integer('status')->default(0);
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('verified')->default(0);
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groups');
     }
 }
