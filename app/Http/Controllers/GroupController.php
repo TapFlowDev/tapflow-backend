fd087e1287e920e14147705e3f12a4bfae03a493
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-10-24 13:18:45 +0300

    upload apis models routes

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Group;
+use app\Models\Company;
+use app\Models\Team;
+use app\Models\Category;
+use app\Models\SubCategory;
+use app\Models\groups_category;
+use Exception;
 
 class GroupController extends Controller
-{
+{   
+    // get all categories 
+    function get_categories()
+    {
+        // try
+        
+        // {
+            $categories=Category::all();
+            $response = array("data" => array(
+                "message" => "get categories successfully",
+                "status" => "200",
+                "error" => $categories,
+            ));
+
+            return (json_encode($response));
+              
+        // }
+        // catch(Exception $error)
+        // {
+        //     $response = array("data" => array(
+        //         "message" => "There IS Error Occurred",
+        //         "status" => "500",
+        //         "error" => $error,
+        //     ));
+
+        //     return (json_encode($response));
+        // }
+    }
+
     //add row 
-    function Insert(Request $req)
+    function add_group(Request $req)
     {
 
     }
