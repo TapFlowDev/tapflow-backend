889be73941269f0abc61699e30a10e568499b3ae
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-11-03 14:33:22 +0200

    update all the api status

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,21 +1,28 @@
+<?php
 
+ namespace App\Http\Middleware;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-// namespace App\Http\Middleware;
-
-//use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
-// class Authenticate extends Middleware
-// {
-//     /**
-//      * Get the path the user should be redirected to when they are not authenticated.
-//      *
-//      * @param  \Illuminate\Http\Request  $request
-//      * @return string|null
-//      */
-//     protected function redirectTo($request)
-//     {
-//         if (! $request->expectsJson()) {
-//             return "status:500 kol";
-//         }
-//     }
-// } 
+ class Authenticate extends Middleware
+ {
+     /**
+     * Get the path the user should be redirected to when they are not authenticated.
+    *
+    * @param  \Illuminate\Http\Request  $request
+      * @return string|null
+     */
+     protected function redirectTo($request)
+    {
+    //    if (! $request->expectsJson()) {
+    //         return "status:500 kol";
+    //     }
+    if(!$request)
+    {
+        $response = array("data" => array(
+            "message" => "Validation Error",
+            "status" => "101",
+        ));
+        return (json_encode($response));
+    }
+    }
+ } 
