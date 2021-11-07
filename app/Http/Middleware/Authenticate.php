08e02d6333a2742e14f89c9bcd97a81fb610ea7e
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-11-07 14:29:07 +0200

    updaet logout

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,8 +15,8 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         dd($request);
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        // if (! $request->expectsJson()) {
+        //     return route('login');
+        // }
     }
 }
