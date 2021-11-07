a4fd2f6f32d319457122b3c5c9a23b4c8e294532
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-11-07 15:02:36 +0200

    updaet logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -254,7 +254,7 @@ class UserController extends Controller
     function logout(Request $req)
     { 
         
-        $token=$req->header('Authorization');
+        $token=$req->route('Authorization');
         dd($token);
         $token=substr($token,7);
         // echo($token);
