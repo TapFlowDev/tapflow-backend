5a7f09d6d63f47a166af7ca03b90ebb5ca83acc8
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-11-09 15:40:27 +0200

    add client and freelancer function to controllers

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,14 +5,34 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Client;
+use App\Models\User;
 
-
+use Exception;
 class ClientController extends Controller
 {
     //add row 
-    function Insert(Request $req)
+    function get_client_info($id)
     {
-
+        try{
+            $user=User::where('id',$id)->get();
+            $client=Client::where('user_id',$id)->get();
+            $response = array("data" => array(
+                "user" => $user,
+                "info" =>$client,
+                "status" => "200",
+            ));
+            return (json_encode($response));
+        }
+        catch(Exception $error)
+        {
+            $response = array("data" => array(
+                "message" => "There IS Error Occurred",
+                "status" => "500",
+                "error" => $error,
+            ));
+    
+            return (json_encode($response));   
+        }
     }
     //update row according to row id
     function Update($id)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,120 +79,8 @@ class UserController extends Controller
         }
     }
 
-    function Insert_freelancer(Request $req)
-    {
-        $userCategoryObj = new UserCategoriesController;
-       
-        $rules = array(
-            "user_id" => "required",
-            "bio" => "required",
-            "hourly_rate" => "required",
-            "attachment" => "required",
-            "image" => "required",
-            "country" => "required",
-            "gender" => "max:1",
-        );
-        $validator = Validator::make($req->all(), $rules);
-        if ($validator->fails()) {
-            $response = array("data" => array(
-                "message" => "Validation Error",
-                "status" => "101",
-                "error" => $validator->errors()
-            ));
-            return (json_encode($response));
-        }
-        try {
-            $data = $req->except(['gender', 'dob', 'category']);
-
-            // print_r($data);
-            $freelancer = Freelancer::create($req->except(['gender', 'dob']));
-            
-            $user = User::find($req->user_id);
-            $user->gender = $req->gender;
-            $user->dob = $req->dob;
-            
-            $user->save();
-            // foreach($req->category as $key => $value){
-            //     // dd($value);
-            //     $userCategoryObj->Insert($value, $req->user_id);
-
-            // }
-            foreach ($req->category as $key => $value) {
-                $categoryArr = array();
-               foreach ($value['subId'] as $keySub => $subValue) {
-                   $categoryArr[$keySub]['user_id'] = $req->user_id;
-                   $categoryArr[$keySub]['category_id'] = $value['catId'];
-                   $categoryArr[$keySub]['sub_category_id'] = $subValue;
-               }
-               $userCategoryObj->addMultiRows($categoryArr);
-           }
-            $response = array("data" => array(
-                "message" => "user information added successfully",
-                "status" => "200",
-                "user_id" => $req->user_id,
-            ));
-
-            return (json_encode($response));
-        } catch (Exception $error) {
-
-            $response = array("data" => array(
-                "message" => "There IS Error Occurred",
-                "status" => "500",
-                "error" => $error,
-            ));
-
-            return (json_encode($response));
-        }
-    }
-    // add company
-    function Insert_client(Request $req)
-    {
-        
-        $rules = array(
-            "user_id" => "required",
-            "bio" => "required",
-            "role" => "required",
-            "attachment" => "required",
-            "image" => "required",
-            "country" => "required",
-
-        );
-        $validator = Validator::make($req->all(), $rules);
-        if ($validator->fails()) {
-            $response = array("data" => array(
-                "message" => "Validation Error",
-                "status" => "101",
-                "error" => $validator->errors()
-            ));
-            return (json_encode($response));
-        }
-        try {
-            $data = $req->except(['gender', 'dob']);
-            $client = Client::create($data);
-            $user = User::find($req->user_id);
-            $user->dob = $req->dob;
-            $user->gender = $req->gender;
-         
-            $user->save();
-
-            $response = array("data" => array(
-                "message" => "user information added successfully",
-                "status" => "200",
-                "user_id" => $req->user_id,
-            ));
-
-            return (json_encode($response));
-        } catch (\Exception $error) {
-
-            $response = array("data" => array(
-                "message" => "There IS Error Occurred",
-                "status" => "500",
-                "error" => $error,
-            ));
-
-            return (json_encode($response));
-        }
-    }
+   
+   
     //login dunction using Sanctum auth token
     function login(Request $req)
     {
@@ -285,53 +173,9 @@ class UserController extends Controller
     }
     
     }
-    //get free lancer info by id
-    function get_freelancer_info($id)
-    {   try{
-        $user=User::where('id',$id)->get();
-        $freelancer=Freelancer::where('user_id',$id)->get();
-        $response = array("data" => array(
-            "user" => $user,
-            "info" =>$freelancer,
-            "status" => "200",
-        ));
-        return (json_encode($response));
-    }
-    catch(Exception $error)
-    {
-        $response = array("data" => array(
-            "message" => "There IS Error Occurred",
-            "status" => "500",
-            "error" => $error,
-        ));
-
-        return (json_encode($response));   
-    }
-    }
+  
     //get client info by id
-    function get_client_info($id)
-    {
-        try{
-            $user=User::where('id',$id)->get();
-            $client=Client::where('user_id',$id)->get();
-            $response = array("data" => array(
-                "user" => $user,
-                "info" =>$client,
-                "status" => "200",
-            ));
-            return (json_encode($response));
-        }
-        catch(Exception $error)
-        {
-            $response = array("data" => array(
-                "message" => "There IS Error Occurred",
-                "status" => "500",
-                "error" => $error,
-            ));
-    
-            return (json_encode($response));   
-        }
-    }
+  
     function getAllUsers(){
         return User::all();
     }
