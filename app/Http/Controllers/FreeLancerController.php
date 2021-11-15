c67ddb19c03784635b26992690805537c7734978
abarabarawi <61247884+abarbarawi@users.noreply.github.com>
2021-11-15 16:23:21 +0200

    ignore

--- app/Http/Controllers/FreeLancerController.php
+++ app/Http/Controllers/FreeLancerController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Exception;
 use Illuminate\Support\Facades\DB;
+
 class FreeLancerController extends Controller
 {
     //add row 
@@ -17,26 +18,26 @@ class FreeLancerController extends Controller
         $userCategoryObj = new UserCategoriesController;
 
         $rules = array(
-            "user_id" => "required",
+            "user_id" => "required|exists:users,id",
             "bio" => "required",
             "hourly_rate" => "required",
-            "attachment" => "required",
-            "image" => "required",
             "country" => "required",
             "gender" => "max:1",
         );
         $validator = Validator::make($req->all(), $rules);
         if ($validator->fails()) {
-            $response = array("data" => array(
-                "message" => "Validation Error",
-                "status" => "101",
-                "error" => $validator->errors()
-            ));
-            return (json_encode($response));
+            // $response = array("data" => array(
+            //     "message" => "Validation Error",
+            //     "status" => "101",
+            //     "error" => $validator->errors()
+            // ));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(101, 'Validation Error', $validator->errors());
+            return json_encode($response);
         }
         try {
             $data = $req->except(['gender', 'dob', 'category']);
-
+            $userId = $req->user_id;
             // print_r($data);
             $freelancer = Freelancer::create($req->except(['gender', 'dob']));
 
@@ -59,22 +60,43 @@ class FreeLancerController extends Controller
                 }
                 $userCategoryObj->addMultiRows($categoryArr);
             }
-            $response = array("data" => array(
-                "message" => "user information added successfully",
-                "status" => "200",
+            if ($req->hasFile('image')) {
+                $destPath = 'images/companies';
+                $ext = $req->file('image')->extension();
+                $imageName = "company-image-" . $userId . "." . $ext;
+                $image = $req->image;
+                $image->move(public_path($destPath), $imageName);
+                $this->updateFiles($userId, $imageName, 'image');
+            }
+            if ($req->hasFile('attachment')) {
+                $destPath = 'images/companies';
+                $ext = $req->file('attachment')->extension();
+                $attachName = "company-attachment-" . $userId . "." . $ext;
+                $attach = $req->attachment;
+                $attach->move(public_path($destPath), $attachName);
+                $this->updateFiles($userId, $attachName, 'attachment');
+            }
+            // $response = array("data" => array(
+            //     "message" => "user information added successfully",
+            //     "status" => "200",
+            //     "user_id" => $req->user_id,
+            // ));
+            // return (json_encode($response));
+            $responseData = array(
                 "user_id" => $req->user_id,
-            ));
-
-            return (json_encode($response));
+            );
+            $response = Controller::returnResponse(200, 'user information added successfully', $responseData);
+            return json_encode($response);
         } catch (Exception $error) {
 
-            $response = array("data" => array(
-                "message" => "There IS Error Occurred",
-                "status" => "500",
-                "error" => $error,
-            ));
-
-            return (json_encode($response));
+            // $response = array("data" => array(
+            //     "message" => "There IS Error Occurred",
+            //     "status" => "500",
+            //     "error" => $error,
+            // ));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(200, 'There Is Error Occurred', $error);
+            return json_encode($response);
         }
     }
     //get free lancer info by id
@@ -83,26 +105,30 @@ class FreeLancerController extends Controller
         try {
             // $user = User::where('id', $id)->get();
             // $freelancer = Freelancer::where('user_id', $id)->get();
-            $user =DB::table('users')
-            ->join('freelancers','users.id','=','freelancers.user_id')
-            ->where('users.id',$id)
-            ->get();
+            $user = DB::table('users')
+                ->leftJoin('freelancers', 'users.id', '=', 'freelancers.user_id')
+                ->where('users.id', $id)
+                ->get();
 
-            $response = array("data" => array(
-                "user"=>$user,
-                "status" => "200",
-            ));
-            return (json_encode($response));
+            // $response = array("data" => array(
+            //     "user"=>$user,
+            //     "status" => "200",
+            // ));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(200, 'data found', $user);
+            return json_encode($response);
         } catch (Exception $error) {
-            $response = array("data" => array(
-                "message" => "There IS Error Occurred",
-                "status" => "500",
-                "error" => $error,
-            ));
+            // $response = array("data" => array(
+            //     "message" => "There IS Error Occurred",
+            //     "status" => "500",
+            //     "error" => $error,
+            // ));
 
-            return (json_encode($response));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(500, 'There IS Error Occurred', $error);
+            return json_encode($response);
+        }
     }
-}
 
 
     //update row according to row id
@@ -118,5 +144,10 @@ class FreeLancerController extends Controller
     function updateTeamId($userId, $teamId)
     {
         Freelancer::where('user_id', $userId)->update(['team_id' => $teamId]);
+    }
+
+    function updateFiles($userId, $imageName, $filedName)
+    {
+        Freelancer::where('user_id', $userId)->update(array($filedName => $imageName));
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
- 
+use App\Models\Freelancer;
+use App\Models\Client;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
@@ -62,15 +63,28 @@ class UserController extends Controller
             "last_name" => "required|max:255",
             "email" => "email|required|max:255|unique:users",
             "password" => "required|min:8|max:255",
-            "type"=>"required|max:1|between:1,2",
+            "type" => "required|max:1|between:1,2",
         );
         $validator = Validator::make($req->all(), $rules);
         if ($validator->fails()) {
+
             $responseData=$validator->errors();
             $response=Controller::returnResponse(101, "Validation Error", $responseData);
             return (json_encode($response));
+
+            // $response = array("data" => array(
+            //     "message" => "Validation Error",
+            //     "status" => "101",
+            //     "error" => $validator->errors()
+            // ));
+            // return (json_encode($response));
+
+            $response = Controller::returnResponse(101, 'Validation Error', $validator->errors());
+            return json_encode($response);
+
         } else {
             try {
+
                 $user=User::create($req->all());
                $responseData= $this->internal_login($req->email,$req->password);
                 $response=Controller::returnResponse( 200,"user added successfully", $responseData);
@@ -81,12 +95,12 @@ class UserController extends Controller
                 return (json_encode($response));
             }                                                           
         }
-    }
-    /*fgfdg*/
+    
+            
+        }
 
-   
-   
-    //login function using Sanctum auth token
+    //login dunction using Sanctum auth token
+
     function login(Request $req)
     {
 
@@ -97,6 +111,7 @@ class UserController extends Controller
             );
             $validator = Validator::make($req->all(), $rules);
             if ($validator->fails()) {
+
                 $response = array("data" => array(
                     "message" => "Validation Error",
                     "status" => "101",
@@ -112,18 +127,52 @@ class UserController extends Controller
                 $responseData = array();
                 $response=Controller::returnResponse( 422,"Unauthorized Error", $responseData);
                 return (json_encode($response));
+
+                $response = Controller::returnResponse(101, 'Validation Error', $validator->errors());
+                return json_encode($response);
+                // $response = array("data" => array(
+                //     "message" => "Validation Error",
+                //     "status" => "101",
+                //     "error" => 
+                // ));
+
+                // return json_encode($response);
+            }
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                // $response = array("data" => array(
+                //     "message" => "Unauthorized",
+                //     "status" => "422",
+                // ));
+                $response = Controller::returnResponse(422, 'Unauthorized', array());
+                return json_encode($response);
+
+                // return (json_encode($response));
+
             }
 
             $user = User::where('email', $req->email)->first();
             if (!Hash::check($req->password, $user->password)) {
+
                 $responseData = array();
                 $response=Controller::returnResponse( 422,"The Password does not match", $responseData);
                 return (json_encode($response));
+
+                // $response = array("data" => array(
+                //     "message" => "The Password does not match",
+                //     "status" => "422",
+                // ));
+                $response = Controller::returnResponse(422, 'The Password does not match', array());
+                return json_encode($response);
+
+                // return (json_encode($response));
+
             }
             $tokenResult = $user->createToken('authToken')->plainTextToken;
             $user->token = $tokenResult;
-            $user_type=$user->type;
+            $user_type = $user->type;
             $user->save();
+
             //check the user info is filed or not 
             
             $responseData = array("data" => array(
@@ -138,57 +187,56 @@ class UserController extends Controller
             $responseData = array("error" => $error,);
             $response=Controller::returnResponse( 500,"There IS Error Occurred", $responseData);
             return (json_encode($response));
-        }
+
+        } 
     }
     function signout(Request $req)
-    { 
-        
-        try{
-        $user= User::find($req->user_id);
-        $user->token="Null";
-        $user->save();
-        $token=DB::table('personal_access_tokens')->where('tokenable_id', $req->user_id)->delete();
-        $response = array("data" => array(
-            "message" => "Logout successfully", 
-            "status" => "200",
-        ));
-
-        return (json_encode($response));
-       
-    }
-    catch(Exception $error)
     {
-        $response = array("data" => array(
-            "message" => "something wrong", 
-            "status" => "500",
-            "error"=>$error,
-        ));
 
-        return (json_encode($response));
+        try {
+            $user = User::find($req->user_id);
+            $user->token = "Null";
+            $user->save();
+            $token = DB::table('personal_access_tokens')->where('tokenable_id', $req->user_id)->delete();
+            // $response = array("data" => array(
+            //     "message" => "Logout successfully",
+            //     "status" => "200",
+            // ));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(200, 'Logout successfully', array());
+            return json_encode($response);
+        } catch (Exception $error) {
+            // $response = array("data" => array(
+            //     "message" => "something wrong",
+            //     "status" => "500",
+            //     "error" => $error,
+            // ));
+            // return (json_encode($response));
+            $response = Controller::returnResponse(500, 'something wrong', $error);
+            return json_encode($response);
+        }
+    }
 
-    }
-    
-    }
-  
     //get client info by id
-  
-    function getAllUsers(){
+
+    function getAllUsers()
+    {
         return User::all();
     }
     //update row according to row id
     function Update($id)
     {
-        
     }
     //delete row according to row id
     function Delete($id)
     {
-        
-    } 
-    
-    function getUserById($id){
+    }
+
+
+
+    function getUserById($id)
+    {
         return User::find($id)->first();
     }
 
-   
 }
