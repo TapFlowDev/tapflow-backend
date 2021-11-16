553ed2db313dfea2d02ba3a09c377d5d0c18e86e
abarabarawi <barbarawiahmad07@gmail.com>
2021-11-16 16:51:13 +0200

    add tasks

---
+++ database/migrations/2021_11_16_144413_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('assign_to',255);
+            $table->text('description');
+            $table->integer('project_id');
+            $table->integer('milestone_id');
+            $table->integer('final_proposal _id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
