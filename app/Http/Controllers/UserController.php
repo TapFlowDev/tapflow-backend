c6361683ff604b18bc09c4f1728a5c804b655d08
abarabarawi <barbarawiahmad07@gmail.com>
2021-11-16 12:08:35 +0200

    clear code2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,32 +123,13 @@ class UserController extends Controller
                 $responseData = array();
                 $response=Controller::returnResponse( 422,"Unauthorized Error", $responseData);
                 return (json_encode($response));
-                $response = Controller::returnResponse(101, 'Validation Error', $validator->errors());
-                return json_encode($response);
             }
-            $credentials = request(['email', 'password']);
-            if (!Auth::attempt($credentials)) {
-                
-                $response = Controller::returnResponse(422, 'Unauthorized', array());
-                return json_encode($response);
-
-             
-
-            }
-
             $user = User::where('email', $req->email)->first();
             if (!Hash::check($req->password, $user->password)) {
 
                 $responseData = array();
                 $response=Controller::returnResponse( 422,"The Password does not match", $responseData);
                 return (json_encode($response));
-
-               
-                $response = Controller::returnResponse(422, 'The Password does not match', array());
-                return json_encode($response);
-
-                
-
             }
             $tokenResult = $user->createToken('authToken')->plainTextToken;
             $user->token = $tokenResult;
