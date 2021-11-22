7625320ef71a863c6065a2534a2c3f5f03da51a9
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2021-11-22 13:51:28 +0200

    sef

--- app/Http/Controllers/FreeLancerController.php
+++ app/Http/Controllers/FreeLancerController.php
@@ -18,7 +18,7 @@ class FreeLancerController extends Controller
     //add row 
     function Insert_freelancer(Request $req)
     {
-        return(($req->categories));
+        return(($req));
         $userCategoryObj = new UserCategoriesController;
         $rules = array(
             "user_id" => "required|exists:users,id",
