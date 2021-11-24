ed66d26e8639f6a394db386e7feff00f7240875e
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2021-11-24 12:15:02 +0200

    filesys

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,7 +33,16 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            
+            'permissions' => [
+                'file' => [
+                    'public' => 0664,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
@@ -53,6 +62,10 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
+        'uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/images',
+        ]
 
     ],
 
