5b16461f5070f11a868d476b75864b4f548f752c
abarabarawi <barbarawiahmad07@gmail.com>
2021-11-24 14:07:04 +0200

    solve conflict

--- database/migrations/2021_11_21_094424_create_final_proposals_table.php
+++ database/migrations/2021_11_21_094424_create_final_proposals_table.php
@@ -15,11 +15,10 @@ class CreateFinalProposalsTable extends Migration
     {
         Schema::create('final_proposals', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
-            $table->integer('propsal_id');
+            $table->integer('proposal_id');
             $table->integer('team_id');
             $table->integer('project_id');
-            $table->string('price_min','255');
-            $table->string('price_max','255');
+            $table->string('price','255');
             $table->text('description')->nullable();
             $table->string('days','255');
             $table->integer('status')->default(0);
