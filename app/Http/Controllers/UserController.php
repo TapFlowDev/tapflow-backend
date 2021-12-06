e234b0bff474c4d80402d6af39d5c93aaf3a4cce
abarabarawi <barbarawiahmad07@gmail.com>
2021-12-06 17:42:19 +0200

    sa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -231,7 +231,7 @@ class UserController extends Controller
     {
         try {
             $user = User::where('id', $req->user_id)->update(["first_name" => $req->first_name, "last_name" => $req->last_name]);
-            dd($user);
+            
             $response = Controller::returnResponse(200, 'User information updated successfully', $user);
             return json_encode($response);
         } catch (Exception $error) {
