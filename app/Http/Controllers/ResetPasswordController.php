c6ce1f21d6d2649926214c532a28ed6275167c69
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2021-12-30 12:54:09 +0200

    reset password

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -84,7 +84,7 @@ class ResetPasswordController extends Controller
                 // $userObj = new User;
                 $user = User::where('email', $userData->email)->get()->first();
                 $user->password = $request->password;
-                $user->tokens()->where('tokenable_id ', $user->id)->delete();
+                $user->tokens()->delete();
                 $user->save();
                 DB::table('password_resets')->where('email', $userData->email)->delete();
             }
