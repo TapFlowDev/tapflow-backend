7c26231da282bf5bc333003776daedc110dbf2d8
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-01-31 11:54:12 +0200

    delete agencies in admin tool

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Hash;
-use Laravel\Cashier\Billable;
+// use Laravel\Cashier\Billable;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Billable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
