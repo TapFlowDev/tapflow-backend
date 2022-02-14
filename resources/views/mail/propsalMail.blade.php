9fbb82428e3506ac03b8382a3cbab71ea826fe48
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-02-14 14:48:41 +0200

    proposal email details

--- resources/views/mail/propsalMail.blade.php
+++ resources/views/mail/propsalMail.blade.php
@@ -1,8 +1,5 @@
-@component('mail::message')
-Propsal id :
-{{ $details['propsal_id'] }}
-
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
+<p style="color: black;">
+Agency {{ $details['team_name'] }} applied to project made by company {{ $details['company_name'] }} with the title {{ $details['project_name'] }}
+<br>
+<span style="color: black;">{{ config('app.name') }},</span>
+</p>
