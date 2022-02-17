0be1e6b0591baa9efef0602a9f4f05fa32b28b20
abarabarawi <barbarawiahmad07@gmail.com>
2022-02-17 10:59:57 +0200

    stop mailchaimpmailchimp

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -212,7 +212,8 @@ class   GroupController extends Controller
                 // ));
 
                 // return (json_encode($response));
-
+                // $mailchimpUserType = 'agency-member';
+                // Newsletter::subscribeOrUpdate($userData->email, ['FNAME' => $userData->first_name, 'LNAME' => $userData->last_name, 'ROLE' => $userData->role, "UTYPE" => $mailchimpUserType, 'ADMIN'=>'admin'], 'Tapflow');
                 $responseData = array(
                     "group_id" => $group_id
                 );
@@ -318,6 +319,9 @@ class   GroupController extends Controller
             $responseData = array(
                 "group_id" => $group_id
             );
+            // $mailchimpUserType = 'agency-member';
+            // Newsletter::subscribeOrUpdate($userData->email, ['FNAME' => $userData->first_name, 'LNAME' => $userData->last_name, 'ROLE' => $userData->role, "UTYPE" => $mailchimpUserType, 'ADMIN'=>'admin'], 'Tapflow');
+           
             $response = Controller::returnResponse(200, 'company added successfully', $responseData);
             return json_encode($response);
         } catch (Exception $error) {
