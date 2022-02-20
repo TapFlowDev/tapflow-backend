4badad3bd23e3aad69556636cf006bee44669233
abarabarawi <barbarawiahmad07@gmail.com>
2022-02-20 17:06:15 +0200

    alter table tasks

---
+++ database/migrations/2022_02_20_150418_alter_tablel_tasks.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTablelTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+          //
+          Schema::table('tasks', function (Blueprint $table) {
+           
+            $table->string('name')->nullable()->change();
+            $table->string('description')->nullable()->change();
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('name')->nullable()->change();
+            $table->string('description')->nullable()->change();
+        });
+    }
+}
