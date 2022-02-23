c95e499e13fcf796667490154dab229b12abdeca
abarabarawi <barbarawiahmad07@gmail.com>
2022-02-23 17:32:17 +0200

    add privileges to get client info

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -163,6 +163,7 @@ class ClientController extends Controller
         foreach ($users as $keyUser => &$user) {
             $categories = $userCategoryObj->getUserCategoriesByUserId($user->id);
             $user->categories = $categories;
+            $user->privileges =$membersObj->getUserPrivileges($user->id);
             $links = User_link::select('link')->where('user_id', $user->id)->get();
             if (count($links) > 0) {
                 // $user->links = $links;
