9ed5f320550d4e13ef2fa322fa27582f5c1d121f
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-02-24 23:18:46 +0200

    Update ProjectCategoriesController.php

--- app/Http/Controllers/ProjectCategoriesController.php
+++ app/Http/Controllers/ProjectCategoriesController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use App\Models\projects_category;
 use Exception;
 use Illuminate\Support\Facades\DB;
-use App\Models\Category;
 
 class ProjectCategoriesController extends Controller
 {
