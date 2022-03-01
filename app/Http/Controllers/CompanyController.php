b7cb5c6472782e34abc02259a4251e40847b74d6
abarabarawi <barbarawiahmad07@gmail.com>
2022-03-01 12:54:12 +0300

    company profile update add validation

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\ProjectController;
 use App\Models\Group;
 use Illuminate\Support\Facades\DB;
 use Exception;
+use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
 {
@@ -93,25 +94,36 @@ class CompanyController extends Controller
     function updateCompanyBio(Request $req)
     {
         try {
-            $userData = Controller::checkUser($req);
-            if ($userData['exist'] == 1) {
-                if ($userData['group_id'] == $req->id) {
-                    if ($userData['privileges'] == 1) {
+            $rules = array(
+                "id" => "required",
+                "bio" => "required",
+            );
+            $validators = Validator::make($req->all(), $rules);
+            if ($validators->fails()) {
+                $responseData = $validators->errors();
+                $response = Controller::returnResponse(101, "Validation Error", $responseData);
+                return (json_encode($response));
+            } else {
+                $userData = Controller::checkUser($req);
+                if ($userData['exist'] == 1) {
+                    if ($userData['group_id'] == $req->id) {
+                        if ($userData['privileges'] == 1) {
 
-                        Company::where('group_id', $req->id)->update(['bio' => $req->bio]);
-                        $response = Controller::returnResponse(200, "successful", []);
-                        return (json_encode($response));
+                            Company::where('group_id', $req->id)->update(['bio' => $req->bio]);
+                            $response = Controller::returnResponse(200, "successful", []);
+                            return (json_encode($response));
+                        } else {
+                            $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                            return (json_encode($response));
+                        }
                     } else {
-                        $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                        $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
                         return (json_encode($response));
                     }
                 } else {
-                    $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
+                    $response = Controller::returnResponse(422, "the user does not have company", []);
                     return (json_encode($response));
                 }
-            } else {
-                $response = Controller::returnResponse(422, "the user does not have company", []);
-                return (json_encode($response));
             }
         } catch (Exception $error) {
             $response = Controller::returnResponse(500, "something went wrong", $error->getMessage());
@@ -121,25 +133,37 @@ class CompanyController extends Controller
     function updateBasicInfo(Request $req)
     {
         try {
-            $userData = Controller::checkUser($req);
-            if ($userData['exist'] == 1) {
-                if ($userData['group_id'] == $req->id) {
-                    if ($userData['privileges'] == 1) {
-                        Group::where('id', $req->id)->update(['name' => $req->name]);
-                        Company::where('group_id', $req->id)->update(['field' => $req->filed, 'sector' => $req->sector]);
-                        $response = Controller::returnResponse(200, "successful", []);
-                        return (json_encode($response));
+            $rules = array(
+                "id" => "required",
+                "name" => "required",
+                "country" => "required",
+            );
+            $validators = Validator::make($req->all(), $rules);
+            if ($validators->fails()) {
+                $responseData = $validators->errors();
+                $response = Controller::returnResponse(101, "Validation Error", $responseData);
+                return (json_encode($response));
+            } else {
+                $userData = Controller::checkUser($req);
+                if ($userData['exist'] == 1) {
+                    if ($userData['group_id'] == $req->id) {
+                        if ($userData['privileges'] == 1) {
+                            Group::where('id', $req->id)->update(['name' => $req->name]);
+                            Company::where('group_id', $req->id)->update(['field' => $req->filed, 'sector' => $req->sector]);
+                            $response = Controller::returnResponse(200, "successful", []);
+                            return (json_encode($response));
+                        } else {
+                            $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                            return (json_encode($response));
+                        }
                     } else {
-                        $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                        $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
                         return (json_encode($response));
                     }
                 } else {
-                    $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
+                    $response = Controller::returnResponse(422, "the user does not have company", []);
                     return (json_encode($response));
                 }
-            } else {
-                $response = Controller::returnResponse(422, "the user does not have company", []);
-                return (json_encode($response));
             }
         } catch (Exception $error) {
             $response = Controller::returnResponse(500, "something went wrong", $error->getMessage());
@@ -149,25 +173,36 @@ class CompanyController extends Controller
     function updateLink(Request $req)
     {
         try {
-            $userData = Controller::checkUser($req);
-            if ($userData['exist'] == 1) {
-                if ($userData['group_id'] == $req->id) {
-                    if ($userData['privileges'] == 1) {
+            $rules = array(
+                "id" => "required",
+                "link" => "required",
+            );
+            $validators = Validator::make($req->all(), $rules);
+            if ($validators->fails()) {
+                $responseData = $validators->errors();
+                $response = Controller::returnResponse(101, "Validation Error", $responseData);
+                return (json_encode($response));
+            } else {
+                $userData = Controller::checkUser($req);
+                if ($userData['exist'] == 1) {
+                    if ($userData['group_id'] == $req->id) {
+                        if ($userData['privileges'] == 1) {
 
-                        Company::where('group_id', $req->id)->update(['link' => $req->link]);
-                        $response = Controller::returnResponse(200, "successful", []);
-                        return (json_encode($response));
+                            Company::where('group_id', $req->id)->update(['link' => $req->link]);
+                            $response = Controller::returnResponse(200, "successful", []);
+                            return (json_encode($response));
+                        } else {
+                            $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                            return (json_encode($response));
+                        }
                     } else {
-                        $response = Controller::returnResponse(422, "Unauthorized this function for admins only", []);
+                        $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
                         return (json_encode($response));
                     }
                 } else {
-                    $response = Controller::returnResponse(422, "Unauthorized you are trying to access another company data", []);
+                    $response = Controller::returnResponse(422, "the user does not have company", []);
                     return (json_encode($response));
                 }
-            } else {
-                $response = Controller::returnResponse(422, "the user does not have company", []);
-                return (json_encode($response));
             }
         } catch (Exception $error) {
             $response = Controller::returnResponse(500, "something went wrong", $error->getMessage());
