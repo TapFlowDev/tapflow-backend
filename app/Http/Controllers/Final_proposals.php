ae1a8c2b48faa6667bac2fae65bcaf652ba11d5b
abarabarawi <barbarawiahmad07@gmail.com>
2022-03-02 14:53:59 +0300

    some changes finap prop milestones project

--- app/Http/Controllers/Final_proposals.php
+++ app/Http/Controllers/Final_proposals.php
@@ -196,7 +196,7 @@ class Final_proposals extends Controller
     {
         $milestone = new Milestones;
         $final_proposal = Final_proposal::where('project_id',$project_id)
-        ->select('title','user_id','proposal_id','project_id','team_id',
+        ->select('id','title','user_id','proposal_id','project_id','team_id',
         'hours','hourly_rate','price','down_payment','down_payment_value',
         'starting_date','status','created_at'
         )->first();
