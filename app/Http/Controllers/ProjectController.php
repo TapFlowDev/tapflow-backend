d414d4c424cb1b106d97699b73769e84663bf6a1
abarabarawi <barbarawiahmad07@gmail.com>
2022-03-22 17:27:04 +0300

    agency pending project test

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -293,7 +293,7 @@ class ProjectController extends Controller
             $projects = DB::table('proposals')
             ->leftJoin('final_proposals', function ($join) {
                 $join->on('proposals.id', '=', 'final_proposals.proposal_id')
-                    ->where('final_proposals.status', '=', 0)->orWhere('final_proposals.status', '=', 3);
+                    ->where('final_proposals.status', '!=', 1);
             })
             ->join('projects', 'proposals.project_id', '=', 'projects.id')
             ->select('projects.*', 'proposals.status as proposal_status', 'final_proposals.status as final_proposal_status')
