3e3d98c175d89b2e402a5da74ec4479ec288549a
abarabarawi <barbarawiahmad07@gmail.com>
2022-03-27 14:14:50 +0300

    projects orderby updated at

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -292,7 +292,7 @@ class ProjectController extends Controller
         $page = ($offset - 1) * $limit;
         try {
             $projects = DB::table('proposals')
-            ->leftJoin('final_proposals', function ($join) {
+            ->rightJoin('final_proposals', function ($join) {
                 $join->on('proposals.id', '=', 'final_proposals.proposal_id')
                     ->where('final_proposals.status', '!=', 1);
             })
