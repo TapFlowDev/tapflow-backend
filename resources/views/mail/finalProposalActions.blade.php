f86704073448a38019dc7c93eb8a331fccf6a389
abarabarawi <barbarawiahmad07@gmail.com>
2022-04-27 18:21:35 +0300

    final proposal emails

--- resources/views/mail/finalProposalActions.blade.php
+++ resources/views/mail/finalProposalActions.blade.php
@@ -23,7 +23,7 @@
             <td>Project Name:</td>
             <td>{{ $details['project_name'] }}</td>
         </tr>
-        <tbody>
+        </tbody>
     </table>
     <br>
     <a href="https://www.tapflow.app/a-user/main/pending-project/{{ $details['project_id'] }}" target="_blank" style="padding: 8px 12px; border: 1px solid #ffc900;border-radius: 10px;font-family: Helvetica, Arial, sans-serif;font-size: medium; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block; background-color: #E57128">
