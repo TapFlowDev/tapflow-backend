f8f4fe507d200241b75b6d46fcd5b664c404c09d
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-08 18:29:20 +0300

    test FP M

--- app/Http/Controllers/Milestones.php
+++ app/Http/Controllers/Milestones.php
@@ -473,21 +473,25 @@ class Milestones extends Controller
     function SubmitFinal($data, $final_proposal_id, $project_id, $hourly_rate)
     {
         try {
-            // return ['code' => 500, 'msg' => $data];
             
-            $rules = array(
-                "milestone_name" => "required",
-                "milestone_num_hours" => "required",
-                "milestone_price" => "required",
-                "deliverables" => "required",
-                "milestone_description" => "required",
-            );
-            $validators = Validator::make($data, $rules);
-            if ($validators->fails()) {
-                return ['code' => 422, 'msg' => $validators->errors()];
-            } else {
+            // return ['code' => 500, 'msg' => $data[0]];
+            
+            
+          
+            
                 // Milestone::where('final_proposal_id', $final_proposal_id)->delete();
                 foreach ($data as $milestone) {
+                    $rules = array(
+                        "milestone_name" => "required",
+                        "milestone_num_hours" => "required",
+                        "milestone_price" => "required",
+                        "deliverables" => "required",
+                        "milestone_description" => "required",
+                    );
+                    $validators = Validator::make($milestone, $rules);
+                    if ($validators->fails()) {
+                        return ['code' => 422, 'msg' => $validators->errors()];
+                    }
                     if (count($milestone['deliverables']) >= 0) {
                         $deliverables = serialize($milestone['deliverables']);
                     }
@@ -513,7 +517,7 @@ class Milestones extends Controller
                     // $milestone = Milestone::create($data);
                 }
                 return ['code' => 200, 'msg' => 'successful'];
-            }
+            
         } catch (Exception $error) {
             return ['code' => 500, 'msg' => $error->getMessage()];
         }
