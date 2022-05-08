59dade310ca2c4ed297771e52b8a744d914feb3c
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-08 18:10:26 +0300

    test FP M

--- app/Http/Controllers/Milestones.php
+++ app/Http/Controllers/Milestones.php
@@ -473,7 +473,7 @@ class Milestones extends Controller
     function SubmitFinal($data, $final_proposal_id, $project_id, $hourly_rate)
     {
         try {
-           
+            return ['code' => 500, 'msg' => $data];
             $rules = array(
                 "milestone_name" => "required",
                 "milestone_num_hours" => "required",
