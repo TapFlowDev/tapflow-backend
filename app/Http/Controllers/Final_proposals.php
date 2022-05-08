e0941c646f99d91dfb7a8ee7a994902800fa8cdd
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-08 18:17:05 +0300

    test FP M

--- app/Http/Controllers/Final_proposals.php
+++ app/Http/Controllers/Final_proposals.php
@@ -491,7 +491,7 @@ class Final_proposals extends Controller
                                     $price = $this->calculatePrice($req->hours, $req->hourly_rate);
                                     $req['price'] = $price;
                                     $milestones = new Milestones;
-                                    $milestones_submit = $milestones->SubmitFinal($req->milestones, $ifExist['final_proposal_id'], $req->project_id, $req->hourly_rate);
+                                    $milestones_submit = $milestones->SubmitFinal($req->milestones->toArray(), $ifExist['final_proposal_id'], $req->project_id, $req->hourly_rate);
                                     if ($milestones_submit['code'] == 200) {
                                         if ($req->down_payment['status'] == 1) {
                                             $this->downPaymentHandler($req->down_payment, $ifExist['final_proposal_id']);
