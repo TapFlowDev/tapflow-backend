706d60bdc358f2d8b843f6443041346825ea1f9f
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-08 18:08:25 +0300

    asdasd

--- app/Http/Controllers/Milestones.php
+++ app/Http/Controllers/Milestones.php
@@ -472,23 +472,21 @@ class Milestones extends Controller
     }
     function SubmitFinal($data, $final_proposal_id, $project_id, $hourly_rate)
     {
-        // try {
-
-        
-        
+        try {
+           
+            $rules = array(
+                "milestone_name" => "required",
+                "milestone_num_hours" => "required",
+                "milestone_price" => "required",
+                "deliverables" => "required",
+                "description" => "required",
+            );
+            $validators = Validator::make($data, $rules);
+            if ($validators->fails()) {
+                return ['code' => 422, 'msg' => $validators->errors()];
+            } else {
                 // Milestone::where('final_proposal_id', $final_proposal_id)->delete();
                 foreach ($data as $milestone) {
-                    $rules = array(
-                        "milestone_name" => "required",
-                        "milestone_num_hours" => "required",
-                        "milestone_price" => "required",
-                        "deliverables" => "required",
-                        "description" => "required",
-                    );
-                    $validators = Validator::make($data, $rules);
-                    if ($validators->fails()) {
-                        return ['code' => 422, 'msg' => $validators->errors()];
-                    }
                     if (count($milestone['deliverables']) >= 0) {
                         $deliverables = serialize($milestone['deliverables']);
                     }
@@ -512,12 +510,12 @@ class Milestones extends Controller
                     // );
 
                     // $milestone = Milestone::create($data);
-                
+                }
                 return ['code' => 200, 'msg' => 'successful'];
             }
-        // } catch (Exception $error) {
-        //     return ['code' => 500, 'msg' => $error->getMessage()];
-        // }
+        } catch (Exception $error) {
+            return ['code' => 500, 'msg' => $error->getMessage()];
+        }
     }
     function updateMilestonesPrices($hours, $hourly_rate, $final_proposal_id)
     {
