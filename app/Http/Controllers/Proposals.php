7e452e53c36878ec0c4c7bafb8e3987cf2cfaca7
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-11 14:58:09 +0300

    proposals conflict

--- app/Http/Controllers/Proposals.php
+++ app/Http/Controllers/Proposals.php
@@ -80,7 +80,7 @@ class Proposals extends Controller
                             'admin_name' => $companyAdminData->first_name,
                             'proposal' => $proposal,
                             'est' => $estPrice
-                        ];
+                        ]; 
                         Mail::mailer('smtp2')->to('hamzahshajrawi@gmail.com')->send(new ProposalMail($details));
                         // Mail::mailer('smtp2')->to($companyAdminData->email)->send(new ProposalMail($details));
                         // Mail::mailer('smtp2')->to('abed@tapflow.app')->send(new ProposalMail($details));
