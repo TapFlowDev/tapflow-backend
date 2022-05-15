a242a935090ee7fc7c1ddb3e5474856efe29b0d2
abarabarawi <barbarawiahmad07@gmail.com>
2022-05-15 16:07:13 +0300

    updates PDF monthly

--- resources/views/pdf/Draft.blade.php
+++ resources/views/pdf/Draft.blade.php
@@ -18,7 +18,7 @@
                 sans-serif,
                 "Apple Color Emoji",
                 "Segoe UI Emoji";
-                background-image: url('/public/images/client-watermark.png');
+            background-image: url('/images/client-watermark.png');
         }
     </style>
 </head>
@@ -26,33 +26,33 @@
 <body>
     <table role="presentation" style="border:0; color:black;  font-size: medium" cellspacing='0' width="100%">
         <tbody>
-            <tr >
+            <tr>
                 <td colspan="6">
                     <h3>{{$agency_name}}</h3>
                 </td>
             </tr>
             <tr>
-                <td width="10%"><strong>@if ($type == 1)Hours @elseif ($type == 2)Hours/Month @endif</strong></td>
+                <td width="10%"><strong>@if ($type == 1) Hours @elseif ($type == 2) Hours/Month @endif</strong></td>
                 <td width="10%">{{$hours}}</td>
                 <td width="20%"><strong>Final Cost</strong></td>
                 <td width="20%">{{$price}}</td>
                 <td width="20%"><strong>Starting Date</strong></td>
                 <td width="20%">{{$starting_date}}</td>
                 <td width="20%"><strong>Project Scope</strong></td>
-                <td width="20%">@if ($type == 1)Projects Based@elseif ($type == 2)Monthly Monthly Retainer @endif </td>
+                <td width="20%">@if ($type == 1) Projects Based @elseif ($type == 2) Monthly Monthly Retainer @endif </td>
             </tr>
             <tr>
-                <td  colspan="6">
-                    <h3> {{$title}}</h3 >
+                <td colspan="6">
+                    <h3> {{$title}}</h3>
                     <p>{{$description}}</p>
                 </td>
             </tr>
             <tr>
-                <td  colspan="6">
-                    <h4>@if ($type == 1)Milestones@elseif ($type == 2)Months @endif </h4>
+                <td colspan="6">
+                    <h4>@if ($type == 1)Milestones @elseif ($type == 2) Months @endif </h4>
                 </td>
             </tr>
-           {!! $milestones !!}
+            {!! $milestones !!}
         </tbody>
     </table>
 </body>
