2a38867ed0190880bc84ef8387faaaef789f0e56
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-05-19 16:53:31 +0300

    test clientSignUpProcess 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -402,7 +402,7 @@ class UserController extends Controller
                
         //     }
         // }
-        $response = Controller::returnResponse(200, "user added successfully", $req->project);
+        $response = Controller::returnResponse(200, "user added successfully", $req->project['categories']);
         return $response;
         $groupObj = new GroupController;
         $teamObj = new CompanyController;
