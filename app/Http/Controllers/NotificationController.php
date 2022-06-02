0462636d44e237865c0659281abc72f02f910745
abarabarawi <barbarawiahmad07@gmail.com>
2022-06-02 11:32:57 +0300

    print fcm array

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -40,8 +40,7 @@ class NotificationController extends Controller
         $url = 'https://fcm.googleapis.com/fcm/send';
         // $FcmToken = User::whereNotNull('fcm_token')->pluck('fcm_token')->all();
         $FcmToken=User::where('id',$request->to_id)->select('fcm_token')->pluck('fcm_token')->toArray();
-        $response=Controller::returnResponse(200,'successful',$FcmToken);
-        return (json_encode($response));
+ 
         $serverKey = env('FCM_SERVER_KEY');
         
         $data = array(
