3e83d3ecb84f1c9a74887636a8c221be795d7721
abarabarawi <barbarawiahmad07@gmail.com>
2022-06-06 13:23:21 +0300

    change room members constraints

--- app/Http/Controllers/FireBaseNotificationsController.php
+++ app/Http/Controllers/FireBaseNotificationsController.php
@@ -26,8 +26,7 @@ class FireBaseNotificationsController extends Controller
     public function sendFireBaseNotification($request)
     {
         try{
-            $response = Controller::returnResponse(101, "Validation Error", $request);
-            return json_encode($response);
+           dd($request);
         $url = 'https://fcm.googleapis.com/fcm/send';
         // $FcmToken = User::whereNotNull('fcm_token')->pluck('fcm_token')->all();
         // $FcmToken=User::where('id',$request->to_id)->select('fcm_token')->pluck('fcm_token')->toArray();
