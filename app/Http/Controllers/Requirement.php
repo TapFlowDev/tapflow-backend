0baa6db41954f16f4c6e855c9ec12b971dde941e
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-06-15 13:56:15 +0300

    fix categories

--- app/Http/Controllers/Requirement.php
+++ app/Http/Controllers/Requirement.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Requirement as requirementModel ;
+use App\Models\Project;
+use App\Models\Requirement as requirementModel;
+use Exception;
 use Illuminate\Http\Request;
+use Svg\Tag\Rect;
+
 /*
   $table->integer('id')->autoIncrement();
             $table->integer('project_id');
@@ -13,37 +17,51 @@ use Illuminate\Http\Request;
             $table->string('name','255');
             $table->text('description'); 
 */
+
 class Requirement extends Controller
 {
-    //add row 
-    function Insert($data,$project_id,$user_id)
-    {
-      
-      foreach($data as $requirement)
+  //add row 
+  function Insert($data, $project_id, $user_id)
+  {
 
-      {
-        $arr=array(
-          'project_id'=>$project_id,
-          'user_id'=>$user_id,
-          'description'=>$requirement,
-        );
-        requirementModel::create($arr);
+    foreach ($data as $requirement) {
+      $arr = array(
+        'project_id' => $project_id,
+        'user_id' => $user_id,
+        'description' => $requirement,
+      );
+      requirementModel::create($arr);
+    }
+  }
+  //update row according to row id
+  function Update($id)
+  {
+  }
+  //delete row according to row id
+  function Delete($id)
+  {
+  }
+  function getRequirementsByProjectId($id)
+  {
+    $requirements = requirementModel::where('project_id', $id)->select('description')->get();
+
+    return ($requirements);
+  }
+  function getRequirementsByProjectIdSelectedColumns(Request $req, $projectId)
+  {
+    try {
+      $project = Project::select('id')->where('id', '=', $projectId)->first();
+      if (!$project) {
+        $response = Controller::returnResponse(422, 'project does not exsist', []);
+        return (json_encode($response));
       }
+      $requirements = requirementModel::select('id as requirementId', 'description')->where('project_id', '=', $projectId)->get();
+    } catch (Exception $error) {
+      $response = Controller::returnResponse(500, "there is an error", $error->getMessage());
+      return (json_encode($response));
     }
-    //update row according to row id
-    function Update($id)
-    {
+    // $requirements = requirementModel::where('project_id', $id)->select('description')->get();
 
-    }
-    //delete row according to row id
-    function Delete($id)
-    {
-
-    }
-    function getRequirementsByProjectId($id)
-    {
-      $requirements=requirementModel::where('project_id',$id)->select('description')->get();
-     
-      return ($requirements);
-    }
+    return $requirements;
+  }
 }
