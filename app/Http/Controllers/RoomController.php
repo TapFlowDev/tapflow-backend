27dd11ac918c10e5350ceab56b738a14d4be94b5
abarabarawi <barbarawiahmad07@gmail.com>
2022-06-19 12:41:28 +0300

    fix git rooms limit;

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -368,7 +368,7 @@ class RoomController extends Controller
                 return json_encode($response);
             }
             $page = ($offset - 1) * $limit;
-            $ids = RoomMembers::where('user_id', $user_id)->select('room_id')->distinct()->latest()->offset($page)->limit($limit)->get();
+            $ids = RoomMembers::where('user_id', $user_id)->select('room_id')->distinct()->offset($page)->limit($limit)->get();
             dd($ids);
             // $rooms_ids = RoomMembers::where('user_id', $user_id)->select('room_id')->distinct()->pluck('room_id')->toArray();
             $rooms = array();
