6c6d165c613298d2cafb89f8ee005598cd2ff2c5
abarabarawi <barbarawiahmad07@gmail.com>
2022-06-19 17:08:47 +0300

    fix git rooms limit;

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -80,7 +80,7 @@ class ChatController extends Controller
             return json_encode($response);
         }
     }
-    function getRoomLastMessage($room_id)
+    function getRoomLastMessage($room_id, $user_id)
     {
         $lastMessage = Messages::where('room_id', $room_id)->select('body','created_at')->distinct()->latest()
             ->offset(1)->limit(1)->first();
