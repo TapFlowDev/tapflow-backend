cc6028d9db5ce9fb3fc2e0b143c04fe4b20fc51f
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-06-23 17:29:12 +0300

    get final proposal

--- app/Http/Controllers/HireDeveloperFinalProposalController.php
+++ app/Http/Controllers/HireDeveloperFinalProposalController.php
@@ -307,7 +307,6 @@ class HireDeveloperFinalProposalController extends Controller
         if ($teamId > 0) {
             $conditionArray[] = ['team_id', '=', $teamId];
             $contracts = hire_developer_final_proposal::whereIn('proposal_id', $proposalIds)->where($conditionArray)->select('*')->get();
-            return dd($contracts);
             $contractsData = $this->getContractsResources($contracts)->first();
             $returnData = $contractsData; 
         } else {
