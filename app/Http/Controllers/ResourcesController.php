b276a3a0f6e437df0dd564adce7c2fc6f877c4a9
Hamzah-shajrawi <65471947+Hamzah-shajrawi@users.noreply.github.com>
2022-06-27 22:50:25 +0300

    fix proposal counter client

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -38,6 +38,8 @@ class ResourcesController extends Controller
                 $response = Controller::returnResponse(101, "Validation Error", $responseData);
                 return (json_encode($response));
             } else {
+                $response = Controller::returnResponse(200, "success", $req->all());
+                return (json_encode($response));
                 $team_id = hire_developer_final_proposal::where('id', $req->contract_id)->select('team_id')->first()->team_id;
                 if ($team_id != $userData['group_id']) {
                     $response = Controller::returnResponse(401, "unauthorized", []);
